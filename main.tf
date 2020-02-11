08517e3e3fe7c72f21436336b2976c919c113a3d
Ryan <ryan.fee625@gmail.com>
2020-02-11 09:53:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "vsphere" {
 #  vsphere_server = var.vsphere_server
 
   # If you have a self-signed cert
-  allow_unverified_ssl = false
+  allow_unverified_ssl = true
 }
 
 data "vsphere_datacenter" "dc" {
