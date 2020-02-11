fd729e78d14a7edf15368a5362b240d8d128b881
Ryan <ryan.fee625@gmail.com>
2020-02-11 10:13:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "vsphere_datacenter" "dc" {
 
 data "vsphere_virtual_machine" "template" {
   name          = "ubuntu16-base-scalr"
-  datacenter_id = "${data.vsphere_datacenter.datacenter.id}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_datastore" "datastore" {
