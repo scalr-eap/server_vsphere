b91aeab879d03dddb0ed2eecfaa20fa017f35832
Ryan <ryan.fee625@gmail.com>
2020-05-15 09:24:22 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
 variable "vsphere_user" {}
 variable "vsphere_password" {}
 variable "vsphere_server" {}
+variable "cpu" {}
+variable "mem" {}
+variable "disk_size" {
+  default = "20"
+}
+variable "os" {
+  default = "ubuntu64Guest"
+}
