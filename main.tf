a1f0043ebdd8c54442df70183712a67950ab0483
Ryan <ryan.fee625@gmail.com>
2020-05-15 09:12:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "vsphere" {
  user           = var.vsphere_user
  password       = var.vsphere_password
- sphere_server = var.vsphere_server
+ vsphere_server = var.vsphere_server
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
